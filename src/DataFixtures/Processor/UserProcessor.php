a8bed919d1df743a9c68ebe01839cbdfbd75cadd
Maxim Paladi <paladium98@gmail.com>
2020-10-10 00:43:58 +0300

    update security and seeder

--- src/DataFixtures/Processor/UserProcessor.php
+++ src/DataFixtures/Processor/UserProcessor.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\DataFixtures\Processor;
 
 use App\Entity\User;
--- src/DataFixtures/Provider/CollectionProvider.php
+++ src/DataFixtures/Provider/CollectionProvider.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\DataFixtures\Provider;
 
 use Doctrine\Common\Collections\ArrayCollection;
